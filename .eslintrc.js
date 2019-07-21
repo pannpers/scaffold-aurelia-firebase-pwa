34280e8be2e4be4062d8b55ca4433b4cfa148317
pampers <pepperoni9@gmail.com>
2019-07-21 19:45:36 +0900

    chore: change module loader from systemjs to webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,14 +7,17 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module', // allow for the use of imports
     project: './tsconfig.json',
   },
-  plugins: ['prettier'],
+  plugins: [
+    'prettier',
+  ],
   extends: [
+    // 'plugin:import/typescript',
     // https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb-base
     'airbnb-base',
     // https://github.com/typescript-eslint/typescript-eslint/blob/ecc96318f47d821c19513652f262b47b15fd8257/packages/eslint-plugin/src/configs/recommended.json
@@ -23,9 +26,21 @@ module.exports = {
     'prettier',
     'prettier/@typescript-eslint', // disable ESLint rules that would conflict with prettier.
   ],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: [ 'src' ],
+        extensions: [ '.js', '.ts', 'd.ts' ],
+      }
+    }
+  },
   rules: {
     'prettier/prettier': 'error',
+    'no-useless-constructor': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
+    'no-empty-function': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off'
   },
 };
